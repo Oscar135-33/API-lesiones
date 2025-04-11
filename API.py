8c1c36457e6ded0b45bcfef1f89620faed533a1c
Oscar Hinojosa <125847665+Oscar135-33@users.noreply.github.com>
2025-04-11 17:41:28 -0600

    Update2 API.py

--- API.py
+++ API.py
@@ -18,6 +18,11 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+# ✅ Ruta raíz para verificación de Render
+@app.route("/")
+def health_check():
+    return "La API está corriendo correctamente 🚀"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
