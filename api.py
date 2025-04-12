3b4010e2fba0b5d02aa8062bb9f570593914a5a6
Oscar Hinojosa <125847665+Oscar135-33@users.noreply.github.com>
2025-04-11 18:19:22 -0600

    Update6 api.py

--- api.py
+++ api.py
@@ -5,12 +5,10 @@ import os
 
 app = Flask(__name__)
 
-# Cargar modelo
-model = tf.keras.models.load_model("modelo_lesiones.keras")
-
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
+        model = tf.keras.models.load_model("modelo_lesiones.keras")
         data = request.get_json()
         input_data = np.array(data["input"])
         predictions = model.predict(input_data)
@@ -25,4 +23,5 @@ def health_check():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    print(f"Starting server on port {port}...")
     app.run(host="0.0.0.0", port=port)
